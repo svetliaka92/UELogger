a812b02338b4d9e6445e712f27e31bf6ada5c4a6
SSVILENO <SSVILENO@ford.com>
2024-06-15 11:27:26 +0200

    Refactored usage, added error log usage

--- Source/CustomLogger/Public/LogLib.h
+++ Source/CustomLogger/Public/LogLib.h
@@ -1,3 +1,4 @@
 #pragma once
 
-#define S_LOG(LogCat, FormatString , ...) UE_LOG(LogTemp, Warning, TEXT("%S: %s"), __FUNCTION__, *FString::Printf(TEXT(FormatString), ##__VA_ARGS__ ) )
+#define S_LOG(FormatString , ...) UE_LOG(LogTemp, Warning, TEXT("%S: %s"), __FUNCTION__, *FString::Printf(TEXT(FormatString), ##__VA_ARGS__ ) )
+#define S_LOG_ERROR(FormatString , ...) UE_LOG(LogTemp, Error, TEXT("%S: %s"), __FUNCTION__, *FString::Printf(TEXT(FormatString), ##__VA_ARGS__ ) )
